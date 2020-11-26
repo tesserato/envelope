16784255bfeec179dfb638d5d000b45f29e3b37f
tesserato <tesserato@hotmail.com>
2020-11-26 11:28:07 -0300

    generate graphs for the site

--- 06graph.py
+++ 06graph.py
@@ -1,39 +1,77 @@
 '''To be used AFTER signal_envelope is installed via pip'''
 import sys
-sys.path.append("signal_envelope/")
+# sys.path.append("signal_envelope/")
 import numpy as np
 import signal_envelope as se
+from scipy import interpolate
 import plotly.graph_objects as go
 
+name = "brass"
+W, _ = se.read_wav(f"test_samples/{name}.wav")
+amp = np.max(np.abs(W))
+W = 4 * W / amp
+X = np.arange(W.size)
 
+Xpos, Xneg = se.get_frontiers(W, 0)
+E = se.get_frontiers(W, 1)
 
+f = interpolate.interp1d(E, np.abs(W[E]), kind="linear", fill_value="extrapolate")
+E = f(X)
 
-W, _ = se.read_wav("test_samples/piano33.wav")
-Xpos, Xneg = se.get_frontiers(W)
+for i in range(E.size):
+  if E[i] < 0.1:
+    E[i] = 0.1
 
+C = W / E
 
+'''============================================================================'''
+'''                                    PLOT                                    '''
+'''============================================================================'''
 
 fig = go.Figure()
 fig.layout.template ="plotly_white"
 fig.update_layout(
-  xaxis_title="i",
-  yaxis_title="Amplitude",
+  xaxis_title="<b><i>i</i></b>",
+  yaxis_title="<b>Amplitude</b>",
   legend=dict(orientation='h', yanchor='top', xanchor='left', y=1.1),
   margin=dict(l=5, r=5, b=5, t=5),
   font=dict(
-    family="Computer Modern",
-    color="black",
-    size=18
+  family="Computer Modern",
+  color="black",
+  size=18
+  )
+)
+
+fig.update_xaxes(showline=False, showgrid=False, zeroline=False)
+fig.update_yaxes(showline=False, zeroline=False, showgrid=True, gridwidth=1, gridcolor='silver', tickvals=[-4,-3,-2,-1, 0, 1,2,3,4])
+
+fig.add_trace(
+  go.Scatter(
+    name="Signal (<b>w = e</b> ⊙ <b>c</b>)",
+    # x=np.arange(W.size),
+    y=W,
+    mode="lines",
+    line=dict(color="gray",),
   )
 )
 
 fig.add_trace(
   go.Scatter(
-    name="Signal",
-    x=np.arange(W.size),
-    y=W,
+    name="Carrier (<b>c</b>)",
+    # x=np.arange(W.size),
+    y=C,
     mode="lines",
-    line=dict(color="silver",),
+    line=dict(color="rgba(0,0,0,0.4)",),
+  )
+)
+
+fig.add_trace(
+  go.Scatter(
+    name="Envelope (<b>e</b>)",
+    # x=E,
+    y=E,
+    mode="lines",
+    line=dict(width=2, color="blue"),
   )
 )
 
@@ -41,9 +79,10 @@ fig.add_trace(
   go.Scatter(
     name="Positive Frontier",
     x=Xpos,
-    y=np.abs(W[Xpos]),
+    y=W[Xpos],
     mode="lines",
     line=dict(width=1, color="red"),
+    visible = "legendonly"
   )
 )
 
@@ -54,7 +93,10 @@ fig.add_trace(
     y=W[Xneg],
     mode="lines",
     line=dict(width=1, color="red"),
+    visible = "legendonly"
   )
 )
 
+
+fig.write_html(f"site/{name}.html", full_html=False, include_mathjax="cdn", include_plotlyjs="cdn")
 fig.show(config=dict({'scrollZoom': True}))
